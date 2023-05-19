5d73c7434302c5426a5751a3c9bf7424bd162581
vikrantaroraa <arora.veshu@gmail.com>
2023-05-19 22:49:40 +0530

    feat: created folder structure dynamically with array of objects

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import SimpleFadingTab from "./components/SimpleFadingTab";
 import FolderStructure from "./components/FolderStructure";
+import Comments from "./components/Comments";
 
 function App() {
   return (
     <div className="App">
       <SimpleFadingTab />
       <FolderStructure />
+      <Comments />
     </div>
   );
 }
--- src/components/FolderStructure/index.tsx
+++ src/components/FolderStructure/index.tsx
@@ -1,52 +1,129 @@
+// @ts-nocheck
 import React from "react";
 import CollapsibleComponent from "../CollapsibleComponent";
 import styles from "./index.module.css";
 import fileIcon from "../../assets/CollapsibleComponent/file.svg";
 
+const folderStructureData = {
+  folders: [
+    {
+      name: "src",
+      subFolders: [
+        {
+          name: "api",
+        },
+        {
+          name: "assets",
+        },
+        {
+          name: "components",
+          subFolders: [
+            { name: "AddressCard", files: ["index.module.css", "index.tsx"] },
+            {
+              name: "BillingComponent",
+              files: ["index.module.css", "index.tsx"],
+            },
+            { name: "CartProduct", files: ["index.module.css", "index.tsx"] },
+          ],
+        },
+        {
+          name: "context",
+        },
+        {
+          name: "pages",
+          subFolders: [
+            { name: "HomePage", files: ["index.module.css", "index.tsx"] },
+            { name: "AllProducts", files: ["index.module.css", "index.tsx"] },
+          ],
+        },
+        {
+          name: "reducers",
+        },
+        {
+          name: "utils",
+        },
+      ],
+      files: [
+        "App.css",
+        "App.tsx",
+        "index.css",
+        "index.tsx",
+        ".gitignore",
+        "package-lock.json",
+        "package.json",
+        "README.md",
+        "tsconfig.json",
+      ],
+    },
+  ],
+};
+
 function FolderStructure() {
   return (
     <div className={styles["folder-structure"]}>
       <p className={styles["folder-structure-heading"]}>Folder Structure</p>
-      <CollapsibleComponent folderName={"src"}>
-        <CollapsibleComponent folderName={"api"}></CollapsibleComponent>
-        <CollapsibleComponent folderName={"assets"}></CollapsibleComponent>
-        <CollapsibleComponent folderName={"components"}>
-          <CollapsibleComponent folderName={"AddressCard"}>
-            <FileElement fileName="index.module.css" />
-            <FileElement fileName="index.tsx" />
+      <div className={styles["folder-structure-siblings-container"]}>
+        {folderStructureData.folders.map(({ name, files, subFolders }) => (
+          <CollapsibleComponent folderName={name}>
+            {subFolders.map(({ name, subFolders }) => (
+              <CollapsibleComponent folderName={name}>
+                {subFolders?.map(({ name, files }) => (
+                  <CollapsibleComponent folderName={name}>
+                    {files.map((file) => (
+                      <FileElement fileName={file} />
+                    ))}
+                  </CollapsibleComponent>
+                ))}
+              </CollapsibleComponent>
+            ))}
+            {files.map((file) => (
+              <FileElement fileName={file} />
+            ))}
           </CollapsibleComponent>
-          <CollapsibleComponent folderName={"BillingComponent"}>
-            <FileElement fileName="index.module.css" />
-            <FileElement fileName="index.tsx" />
+        ))}
+
+        {/* STATIC HARD CODED FOLDER STRUCTURE - DO NOT DELETE */}
+        <CollapsibleComponent folderName={"src"}>
+          <CollapsibleComponent folderName={"api"}></CollapsibleComponent>
+          <CollapsibleComponent folderName={"assets"}></CollapsibleComponent>
+          <CollapsibleComponent folderName={"components"}>
+            <CollapsibleComponent folderName={"AddressCard"}>
+              <FileElement fileName="index.module.css" />
+              <FileElement fileName="index.tsx" />
+            </CollapsibleComponent>
+            <CollapsibleComponent folderName={"BillingComponent"}>
+              <FileElement fileName="index.module.css" />
+              <FileElement fileName="index.tsx" />
+            </CollapsibleComponent>
+            <CollapsibleComponent folderName={"CartProduct"}>
+              <FileElement fileName="index.module.css" />
+              <FileElement fileName="index.tsx" />
+            </CollapsibleComponent>
           </CollapsibleComponent>
-          <CollapsibleComponent folderName={"CartProduct"}>
-            <FileElement fileName="index.module.css" />
-            <FileElement fileName="index.tsx" />
+          <CollapsibleComponent folderName={"context"}></CollapsibleComponent>
+          <CollapsibleComponent folderName={"pages"}>
+            <CollapsibleComponent folderName={"HomePage"}>
+              <FileElement fileName="index.module.css" />
+              <FileElement fileName="index.tsx" />
+            </CollapsibleComponent>
+            <CollapsibleComponent folderName={"AllProducts"}>
+              <FileElement fileName="index.module.css" />
+              <FileElement fileName="index.tsx" />
+            </CollapsibleComponent>
           </CollapsibleComponent>
+          <CollapsibleComponent folderName={"reducers"}></CollapsibleComponent>
+          <CollapsibleComponent folderName={"utils"}></CollapsibleComponent>
+          <FileElement fileName="App.css" />
+          <FileElement fileName="App.tsx" />
+          <FileElement fileName="index.css" />
+          <FileElement fileName="index.tsx" />
+          <FileElement fileName=".gitignore" />
+          <FileElement fileName="package-lock.json" />
+          <FileElement fileName="package.json" />
+          <FileElement fileName="README.md" />
+          <FileElement fileName="tsconfig.json" />
         </CollapsibleComponent>
-        <CollapsibleComponent folderName={"context"}></CollapsibleComponent>
-        <CollapsibleComponent folderName={"pages"}>
-          <CollapsibleComponent folderName={"HomePage"}>
-            <FileElement fileName="index.module.css" />
-            <FileElement fileName="index.tsx" />
-          </CollapsibleComponent>
-          <CollapsibleComponent folderName={"AllProducts"}>
-            <FileElement fileName="index.module.css" />
-            <FileElement fileName="index.tsx" />
-          </CollapsibleComponent>
-        </CollapsibleComponent>
-        <CollapsibleComponent folderName={"reducers"}></CollapsibleComponent>
-        <CollapsibleComponent folderName={"utils"}></CollapsibleComponent>
-        <FileElement fileName="App.css" />
-        <FileElement fileName="App.tsx" />
-        <FileElement fileName="index.css" />
-        <FileElement fileName="index.tsx" />
-        <FileElement fileName=".gitignore" />
-        <FileElement fileName="package-lock.json" />
-        <FileElement fileName="package.json" />
-        <FileElement fileName="README.md" />
-        <FileElement fileName="tsconfig.json" />
-      </CollapsibleComponent>
+      </div>
     </div>
   );
 }
